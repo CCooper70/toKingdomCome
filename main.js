f605538f320f37c4fd616a6ca4c0e07c6ce9a706
christy cooper betty <ccooper.prsvr@gmail.com>
2024-01-25 10:02:41 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+function setSource(id, url){
+    document.querySelector(`#${id}`).src = url
+}
+
+setSource('image-1', https://media.istockphoto.com/id/1358464345/photo/mare-and-foal.jpg?s=612x612&w=0&k=20&c=K_yKzFMHEWckTpYN-LJMo7jUDdNR5Gndk5ZYSaXnTwI=))
+setSource('image-2', https://cdn.pixabay.com/photo/2014/12/08/17/52/horse-561221_640.jpg))
+setSource('image-3', https://images.photowall.com/interiors/48312/landscape/wallpaper/room50.jpg?w=900&q=80))
+
+
+
+
+
+
+
+
